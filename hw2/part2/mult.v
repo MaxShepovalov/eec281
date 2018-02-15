50881e27f8522a8b9775b91691035e05ccfdc8c4
MaxShepovalov <makson4622gmail.com>
2018-02-14 23:16:59 -0800

    return for reg fixes

--- hw2/part2/mult.v
+++ hw2/part2/mult.v
@@ -43,8 +43,8 @@ endmodule //add42
 
 //main module
 module mult (
-    input reg [7:0] a,
-    input reg [7:0] b,
+    input [7:0] a,
+    input [7:0] b,
     input clk,
     output reg [15:0] out
 );
@@ -92,11 +92,12 @@ module mult (
     reg st2_p4;
     reg [3:0] st2_c1; //carry between top 4:2 adders
     reg [3:0] st2_c2; //carry between bottom 4:2 adders 
-    always @(p0_r or p3_r or p7_r or p4_r) //pass through bits
+    always @(p0_r or p3_r or p7_r or p4_r) begin //pass through bits
         st2_p0[0] = p0_r[0];
         st2_p2[10] = p3_r[7];
         st2_p1[14] = p7_r[7];
         st2_p2[4] = p4_r[0];
+    end
     //top row
     ha st2_ha11 (.a (p0_r[1]), .b (p1_r[0]), .c(st2_p0[2]), .s(st2_p0[1]));
     fa st2_fa11 (.a (p0_r[2]), .b (p1_r[1]), .c (p2_r[0]), .carry (st2_p0[3]), .sum (st2_p1[2]));
@@ -154,7 +155,7 @@ module mult (
     add42 st3_a3 (.a (st2_p0_r[8]),  .b (st2_p1_r[8]),  .c (st2_p2_r[8]),  .d (st2_p3_r[8]),  .ci (st3_c[1]), .cc (st3_c[2]), .co (st3_p0[9]), .s (st3_p1[8]));
     add42 st3_a4 (.a (st2_p0_r[9]),  .b (st2_p1_r[9]),  .c (st2_p2_r[9]),  .d (st2_p3_r[9]),  .ci (st3_c[2]), .cc (st3_c[3]), .co (st3_p0[10]), .s (st3_p1[9]));
     add42 st3_a5 (.a (st2_p0_r[10]), .b (st2_p1_r[10]), .c (st2_p2_r[10]), .d (st2_p3_r[10]), .ci (st3_c[3]), .cc (st3_p1[11]), .co (st3_p0[11]), .s (st3_p1[10]));
-    ha st3_ha3   (.a (st2_p0_r[11]), .b (st2_p1_r[11]), .c (st3_p0[12]), .s (st3_p3[1]));
+    ha st3_ha3   (.a (st2_p0_r[11]), .b (st2_p1_r[11]), .c (st3_p0[12]), .s (st3_p2[1]));
     fa st3_fa3   (.a (st2_p0_r[12]), .b (st2_p1_r[12]), .c (st2_p2_r[11]), .carry (st3_p0[13]), .sum (st3_p1[12]));
     ha st3_ha4   (.a (st2_p0_r[13]), .b (st2_p1_r[13]), .c (st3_p0[14]), .s (st3_p1[13]));
     ha st3_ha5   (.a (st2_p0_r[14]), .b (st2_p1_r[14]), .c (st3_p0[15]), .s (st3_p1[14]));
@@ -174,13 +175,13 @@ module mult (
     reg [15:3] st4_p1;
 
     //pass though bits and add constant
-    always @() begin
+    always @(st3_p0_r or st3_p1_r) begin
         st4_p0[7:0] = st3_p0_r[7:0];
         st4_p1[7:3] = st3_p1_r[7:3];
         //add constant
         st4_p1[8] = 1'b1;
         //add constant (to cut half adder)
-        st4_p1[15] = ~st3_p0[15];
+        st4_p1[15] = ~st3_p0_r[15];
     end
 
     fa st4_fa1 (.a (st3_p0_r[8]), .b (st3_p1_r[8]), .c (st3_p2_r[0]), .carry (st4_p0[9]), .sum (st4_p0[8]));
