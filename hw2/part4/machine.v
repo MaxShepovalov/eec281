4d721fb5a2dd5b260f5cc252b6071d25003a58a6
MaxShepovalov <makson4622gmail.com>
2018-02-19 11:47:17 -0800

    hw2 p4. tests finished

--- hw2/part4/machine.v
+++ hw2/part4/machine.v
@@ -4,14 +4,15 @@ module machine (
     input reset,
     input clk,
     input skip,
-    output reg shutter
+    output reg shutter,
+    output reg [2:0] state
 );
 
     parameter IDLE = 2'b00;
     parameter CAMERA_ON = 2'b01;
     parameter PROCESS = 2'b10;
 
-    reg [2:0] state, state_c;
+    reg [2:0] state_c;
     reg [2:0] count, count_c;
     reg shutter_c;
 
@@ -24,7 +25,8 @@ module machine (
             IDLE: begin
                 if (count == 3'b000) begin
                     state_c = CAMERA_ON;
-                    count_c = 3'b110; //7 cycles
+                    count_c = 3'b101; //6 cycles
+                    shutter_c = 1'b1;
                 end else
                     count_c = count - 3'b001;
             end
@@ -32,22 +34,23 @@ module machine (
                 if (count == 3'b000) begin
                     state_c = PROCESS;
                     count_c = 3'b011; // 4 cycles
+                    shutter_c = 1'b0;
                 end else begin
-                    shutter_c = (count == 3'b110) || (count == 3'b011);
+                    shutter_c = (count == 3'b011);
                     count_c = count - 3'b001;
                 end
             end
             PROCESS: begin
                 if (skip == 1'b1 || count == 3'b000) begin
                     state_c = IDLE;
-                    count_c = 3'b011;
+                    count_c = 3'b010;
                 end else begin
                     count_c = count - 3'b001;
                 end
             end
 
             default: begin
-                count_c = 3'b011;
+                count_c = 3'b010;
                 state_c = IDLE;
                 shutter_c = 1'b0;
             end
@@ -57,7 +60,7 @@ module machine (
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             // reset
-            count <= #1 3'b011;
+            count <= #1 3'b010;
             state <= #1 IDLE;
             shutter <= #1 1'b0;
         end
