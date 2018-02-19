5ab82b4b0a9454af0d7b3b2ba5225c3b0253f894
MaxShepovalov <makson4622gmail.com>
2018-02-19 00:46:53 -0800

    hw2 p4. bug fixes and test results

--- hw2/part4/machine.v
+++ hw2/part4/machine.v
@@ -31,47 +31,16 @@ module machine (
             CAMERA_ON: begin
                 if (count == 3'b000) begin
                     state_c = PROCESS;
-                    count_c = 3'b100; // 4 cycles
+                    count_c = 3'b011; // 4 cycles
                 end else begin
                     shutter_c = (count == 3'b110) || (count == 3'b011);
                     count_c = count - 3'b001;
                 end
-                /*case(count)
-                    3'b000: begin             //cycle 7
-                        state_c = PROCESS;
-                        count_c = 3'b100; // 4 cycles
-                    end
-                    3'b110: begin             //cycle 1
-                        shutter_c = 1'b1;
-                        count_c = count - 3'b001;
-                    end
-                    3'b101: begin             //cycle 2
-                        shutter_c = 1'b0;
-                        count_c = count - 3'b001;
-                    end
-                    3'b100: begin             //cycle 3
-                        shutter_c = 1'b0;
-                        count_c = count - 3'b001;
-                    end
-                    3'b011: begin             //cycle 4
-                        shutter_c = 1'b1;
-                        count_c = count - 3'b001;
-                    end
-                    3'b010: begin             //cycle 5
-                        shutter_c = 1'b0;
-                        count_c = count - 3'b001;
-                    end
-                    3'b001: begin             //cycle 6
-                        shutter_c = 1'b0;
-                        count_c = count - 3'b001;
-                    end
-                    default: shutter_c = 1'b0;
-                endcase*/
             end
             PROCESS: begin
                 if (skip == 1'b1 || count == 3'b000) begin
                     state_c = IDLE;
-                    count = 3'b011;
+                    count_c = 3'b011;
                 end else begin
                     count_c = count - 3'b001;
                 end
@@ -80,6 +49,7 @@ module machine (
             default: begin
                 count_c = 3'b011;
                 state_c = IDLE;
+                shutter_c = 1'b0;
             end
         endcase
     end
@@ -89,6 +59,7 @@ module machine (
             // reset
             count <= #1 3'b011;
             state <= #1 IDLE;
+            shutter <= #1 1'b0;
         end
         else begin
             count <= #1 count_c;
