809dc9d0f70f44ae77564e7fb8f42b6077f98571
MaxShepovalov <makson4622@gmail.com>
2018-03-19 13:13:33 -0700

    fix

--- hw4/probl2/check_vals.m
+++ hw4/probl2/check_vals.m
@@ -6,8 +6,8 @@ A = a / 32768;
 B = b / 32768;
 E = 2* pi * wn_exp / 4096; %angle
 W = exp(1i * E); %fft coef
-Xt = x / 32768; %test result
-Yt = y / 32768; %test result
+Xt = x / 16384; %test result
+Yt = y / 16384; %test result
 Xm = A + B; %matlab result
 Ym = (A - B).*W;
 
